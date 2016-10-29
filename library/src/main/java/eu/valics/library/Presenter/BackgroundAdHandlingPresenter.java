f02803d600cf6c85e82c249eac16a48b6fb7bc16
Lukáš Válik <lukas.valik@zentity.com>
2016-10-29 13:44:06 +0200

    logs to launcher class, it seem it causes problems

--- library/src/main/java/eu/valics/library/Presenter/BackgroundAdHandlingPresenter.java
+++ library/src/main/java/eu/valics/library/Presenter/BackgroundAdHandlingPresenter.java
@@ -2,6 +2,7 @@ package eu.valics.library.Presenter;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.util.Log;
 
 import eu.valics.library.AppInfo;
 import eu.valics.library.SplashActivityLauncher;
@@ -41,7 +42,7 @@ abstract class BackgroundAdHandlingPresenter implements InterstitialAdCreator.In
 
     protected void handleBackgroundStateAndInterstitialAd() {
         if (mAppInfo.wasInBackground() && !mAppInfo.isShowingInterstitialAd()) {
-            //Log.d(TAG, "was in bg, not showing interstitial");
+            Log.d(TAG, "was in bg, not showing interstitial");
 
             int adBuffer = mAppInfo.getBufferForInterstitialAd() + 1;
             mAppInfo.setBufferForInterstitialAd(adBuffer);
@@ -52,30 +53,30 @@ abstract class BackgroundAdHandlingPresenter implements InterstitialAdCreator.In
                     InterstitialAdCreator.get(mContext).setListener(this);
                     mAppInfo.setShowingInterstitialAd(true);
                     InterstitialAdCreator.get(mContext).showInterstatialAd();
-                    //Log.d(TAG, "ad loaded");
+                    Log.d(TAG, "ad loaded");
                 } else if (mAppInfo.isOnline() && !mAppInfo.wasLoadingOfAppIsDone()) {
 
-                    //Log.d(TAG, "online and loading of app not done");
+                    Log.d(TAG, "online and loading of app not done");
 
                     try {
                         SplashActivityLauncher.launch(mContext); // launcher activity should be splash
-                        //Log.d(TAG, "start splash activity");
+                        Log.d(TAG, "start splash activity");
                     } catch (PackageManager.NameNotFoundException e) {
-                        //Log.d(TAG, "splash not found or smt like that");
+                        Log.d(TAG, "splash not found or smt like that");
                         e.printStackTrace();
                     }
-                } /*else if (!mAppInfo.isOnline())
+                } else if (!mAppInfo.isOnline())
                     Log.d(TAG, "is not online");
                 else if (mAppInfo.wasLoadingOfAppIsDone())
-                    Log.d(TAG, "was loading of app is done");*/
+                    Log.d(TAG, "was loading of app is done");
             }
         } else {
             mAppInfo.setShowingInterstitialAd(false);
-            //Log.d(TAG, "was not in bg or showing interstitial");
+            Log.d(TAG, "was not in bg or showing interstitial");
         }
         if (!mAppInfo.isShowingInterstitialAd()) {
             mAppInfo.setLoadingOfAdIsDone(false);
-            //Log.d(TAG, "setting loading ad done to false");
+            Log.d(TAG, "setting loading ad done to false");
         }
     }
 
@@ -109,7 +110,7 @@ abstract class BackgroundAdHandlingPresenter implements InterstitialAdCreator.In
     public void onClosedAd() {
         mAppInfo.setShowingInterstitialAd(false);
         InterstitialAdCreator.get(mContext).removeListener();
-        InterstitialAdCreator.get(mContext).requestNewInterstitial();
+        //InterstitialAdCreator.get(mContext).requestNewInterstitial();
     }
 
     @Override
--- library/src/main/java/eu/valics/library/SplashActivityLauncher.java
+++ library/src/main/java/eu/valics/library/SplashActivityLauncher.java
@@ -4,22 +4,29 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.util.Log;
 
 /**
  * Created by L on 9/7/2016.
  */
 public class SplashActivityLauncher {
 
+    private static final String TAG = "BG_AD_LAUNCHER";
+
     public static void launch(Context context) throws PackageManager.NameNotFoundException {
         final String packageName = context.getPackageName();
+        Log.d(TAG, "packageName: "+ packageName);
         final Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
         if (launchIntent != null) {
+            Log.d(TAG, "launchIntent: not null");
             final String mainActivity = launchIntent.getComponent().getClassName();
+            Log.d(TAG, "mainActivity: " + mainActivity);
             final Intent intent = new Intent(Intent.ACTION_MAIN);
             intent.addCategory(Intent.CATEGORY_LAUNCHER);
             intent.setComponent(new ComponentName(packageName, mainActivity));
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intent);
-        }
+            Log.d(TAG, "after launch: ");
+        } else Log.d(TAG, "launchIntent: is null");
     }
 }
