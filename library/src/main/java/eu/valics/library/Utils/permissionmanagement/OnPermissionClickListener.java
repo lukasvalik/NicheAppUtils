73c8bc8508420cdabaa11483d123f0ee34c21858
Lukas Valik <lukas.valik@zentity.com>
2017-08-24 07:51:38 +0200

    0.5.18 OnPermissionClickListener is anonymous class now and has public constructor

--- library/src/main/java/eu/valics/library/Utils/permissionmanagement/OnPermissionClickListener.java
+++ library/src/main/java/eu/valics/library/Utils/permissionmanagement/OnPermissionClickListener.java
@@ -1,13 +1,47 @@
 package eu.valics.library.Utils.permissionmanagement;
 
-import android.view.View;
-
 import eu.valics.library.Base.BaseActivity;
 
 /**
  * Created by L on 8/24/2017.
  */
 
+public abstract class OnPermissionClickListener {
+
+    private BaseActivity mBaseActivity;
+    private PermissionManager mPermissionManager;
+    private PermissionInvalidationListener mPermissionInvalidationListener;
+
+    public OnPermissionClickListener(BaseActivity baseActivity, PermissionManager permissionManager) {
+        mBaseActivity = baseActivity;
+        mPermissionManager = permissionManager;
+
+        mPermissionInvalidationListener = () -> {
+            mBaseActivity.resumeBgTriggeringInterstitialAd();
+            if (mPermissionManager.enabledAllFatalPermissions())
+                onPermissionGrantedClickListener();
+            else
+                onPermissionNotGrantedClickListener();
+        };
+
+        onPermissionClickListenerInternal();
+    }
+
+    public abstract void onPermissionGrantedClickListener();
+
+    public abstract void onPermissionNotGrantedClickListener();
+
+    private void onPermissionClickListenerInternal() {
+        mBaseActivity.pauseBgTriggeringInterstitialAd();
+        mPermissionManager.setListener(mPermissionInvalidationListener);
+        mPermissionManager.invalidatePermissions(true);
+    }
+}
+
+/**
+ * This is one implementing onClickListener, but i do not need that in dataBinding
+ */
+/*
 public abstract class OnPermissionClickListener implements View.OnClickListener {
 
     private BaseActivity mBaseActivity;
@@ -42,3 +76,4 @@ public abstract class OnPermissionClickListener implements View.OnClickListener 
         onPermissionGrantedClickListenerInternal();
     }
 }
+*/
