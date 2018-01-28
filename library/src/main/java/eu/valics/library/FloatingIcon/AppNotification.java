9f41b99cc11b94eb2db6327a521ecdeb3e2c7271
Lukas Valik <lukassvalik@gmail.com>
2018-01-28 21:05:32 +0100

    Floating icon notification Android O support

--- library/src/main/java/eu/valics/library/FloatingIcon/AppNotification.java
+++ library/src/main/java/eu/valics/library/FloatingIcon/AppNotification.java
@@ -1,12 +1,14 @@
 package eu.valics.library.FloatingIcon;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import eu.valics.library.NicheAppUtils;
@@ -15,6 +17,8 @@ import eu.valics.library.NicheAppUtils;
  * Created by L on 9/7/2016.
  */
 public class AppNotification {
+
+    public static final String PRIMARY_CHANNEL = "default";
 
     private int mIconId;
 
@@ -27,6 +31,13 @@ public class AppNotification {
         mContext = context;
         mManager= (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
         mIconId = NicheAppUtils.getFloatingIconId();
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel chan1 = new NotificationChannel(PRIMARY_CHANNEL,
+                    PRIMARY_CHANNEL, NotificationManager.IMPORTANCE_DEFAULT);
+            chan1.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            mManager.createNotificationChannel(chan1);
+        }
     }
 
     public static AppNotification get(Context context){
@@ -41,8 +52,13 @@ public class AppNotification {
         i.putExtra("EXTRA", "EXTRA");
         PendingIntent pendingIntent = PendingIntent.getService(mContext, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        mBuilder = new NotificationCompat.Builder(mContext)
-                .setSmallIcon(mIconId)
+        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            mBuilder = new NotificationCompat.Builder(mContext);
+        } else {
+            mBuilder = new NotificationCompat.Builder(mContext, PRIMARY_CHANNEL);
+        }
+
+        mBuilder.setSmallIcon(mIconId)
                 .setLargeIcon(createBitmapFromDrawable(mIconId))
                 .setContentTitle(NicheAppUtils.getFloatingNotificationTitle())
                 .setContentIntent(pendingIntent)
@@ -51,7 +67,7 @@ public class AppNotification {
         Notification notification = mBuilder.build();
         notification.flags |= Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
 
-        mManager.notify(NicheAppUtils.getFloatingIconId(), notification);
+        mManager.notify(7337, notification);
 
     }
 
